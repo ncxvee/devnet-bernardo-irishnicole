ba0c92298ed6f56cb8925bfbf2b166a1463dc1e3
Russell Nunag <russellnunagindependenceict@gmail.com>
2026-09-26 13:15:07 +0800

    Removes a movie title from the list

---
+++ midterm/remove_movie.py
@@ -0,0 +1,12 @@
+movies = ["Movie 01", "Movie 02", "Movie 03"]
+
+remove = input ("Remove Movie Title: ")
+
+movies.remove(remove)
+
+print ()
+print("Movie removed successfully.")
+print ()
+
+for x in movies:
+    print(x)
