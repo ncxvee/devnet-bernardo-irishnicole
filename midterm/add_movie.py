8f90e4b13145db90f1559148c801c2dae831065b
Russell Nunag <russellnunagindependenceict@gmail.com>
2026-09-26 12:39:37 +0800

    Created display_menu and add_movie

---
+++ midterm/add_movie.py
@@ -0,0 +1,9 @@
+movies = ["Movie 01", "Movie 02", "Movie 03"]
+
+title = input ("Enter movie title: ")
+director = input ("Enter director: ")
+status = input ("Enter status: ")
+
+movies.append(title)
+
+print("Movie added successfully.")
---
+++ midterm/display_menu.py
@@ -0,0 +1,8 @@
+print (" === Movie Collection Manager === ")
+print (" 1. Add a movie ")
+print (" 2. View all movie ")
+print (" 3. Count watched vs unwatched ")
+print (" 4. Find a movie ")
+print (" 5. Exit ")
+
+option = input ("Choose an option: ")
